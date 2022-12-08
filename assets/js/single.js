d863294898f86cfe38065fea7855a01fb709a041
HazShk <shaikh.hassan01@outlook.com>
2022-12-08 11:06:59 -0500

    Created a link between pages and can read and use data as query parameter and also handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,20 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -17,11 +32,11 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      alert("There was a problem with your request");
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
-getRepoIssues("facebook/react");
 
 var displayIssues = function (issues) {
   //check for issues
@@ -71,3 +86,4 @@ var displayWarning = function (repo) {
   // append to warning container
   limitWarningEl.appendChild(linkEl);
 };
+getRepoName();
